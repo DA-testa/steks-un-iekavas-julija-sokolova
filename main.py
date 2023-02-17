bfef50e7e95d085685528af400334f30487d808f
Jūlija Sokolova <125577251+julija-sokolova@users.noreply.github.com>
2023-02-17 13:30:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # python3
-
+# 221rdb058 Jūlija Sokolova 15. grupa
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
@@ -14,19 +14,22 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i+1))
-            pass
+            
 
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop();
-            pass
+            if opening_brackets_stack:
+                return opening_brackets_stack[0].position
+            return "Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    print(mismatch)
+ 
+            
 
 
 if __name__ == "__main__":
